8dfa0e9e5996ad4a05c4db1bf76a1cc478b9ce0e
rafa4345 <rafa4345@hotmail.com>
2025-07-29 21:38:35 -0400

    Add project files.

---
+++ ToDoApi/Models/Tarea.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ToDoApi.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public bool Completada { get; set; }
+    }
+}
